a08bc0acbabf24c45c7dc7444cc6695c2a1e643c
PedroCSFernandes <128640783+PedroCSFernandes@users.noreply.github.com>
2023-03-22 18:53:12 -0300

    Add files via upload

---
+++ 1 .py
@@ -0,0 +1,16 @@
+a=int(input())   #int() pro computadoe enteder que é um numero inteiro
+c=input()
+b=int(input())    #float() para numeros com virgula
+
+
+if c=="*":
+    print(a*b)
+
+elif c=="/":
+    print(a/b)
+
+elif c=="+":
+    print(a+b)
+
+elif c=="-":
+    print(a-b)

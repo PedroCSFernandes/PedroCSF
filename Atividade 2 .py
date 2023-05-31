0dc729cf3f9d1f2199206146f2703a22658247d3
PedroCSFernandes <128640783+PedroCSFernandes@users.noreply.github.com>
2023-05-31 18:18:10 -0300

    Add files via upload

---
+++ Atividade 2 .py
@@ -0,0 +1,12 @@
+#Objetivo: Criar um programa que printe os numeros de uma lista e, depois, eles multiplicados por 2
+#Bonus: Colocar os numeros multiplicados em uma outra lista e printa-la
+
+import math
+
+lista=(2,3,7,12,2)
+
+for i in lista:
+    
+    
+    print(i*2)
+
